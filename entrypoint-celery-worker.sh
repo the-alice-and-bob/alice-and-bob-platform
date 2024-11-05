8e63b923cb4b92e540c7652e2898e19d784457fe
cr0hn <cr0hn@cr0hn.com>
2024-11-05 16:33:10 +0100

    Reduce Celery worker concurrency to 2.
    Adjusted the concurrency parameter for Celery workers from 10 to 2 to optimize resource allocation for our current system load. This change aims to improve overall performance and system stability in a high-concurrency environment.

--- entrypoint-celery-worker.sh
+++ entrypoint-celery-worker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-celery -A alicebob.celery_app worker -l info --concurrency=10 --pool=gevent --uid=alicebob
+celery -A alicebob.celery_app worker -l info --concurrency=2 --pool=gevent --uid=alicebob
