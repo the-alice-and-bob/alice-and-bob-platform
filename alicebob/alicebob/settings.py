050fb956fb1c09f3ac9628f383f8912b52f9341a
cr0hn <cr0hn@cr0hn.com>
2024-11-21 16:04:38 +0100

    Fix CSRF trusted origin URL in settings
    Update the CSRF_TRUSTED_ORIGINS list to include the full URL with HTTPS for 'awesome-backstage.alicebob.io'. This adjustment ensures proper handling of Cross-Site Request Forgery protection.

--- alicebob/alicebob/settings.py
+++ alicebob/alicebob/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = '7be4a07985cd0492d1fab2b2e3a3f54e9f88747adfd7096149143e4909d5f5e2e3
 DEBUG = decouple.config('DEBUG', default=False, cast=bool)
 CSRF_TRUSTED_ORIGINS = [
     'localhost',
-    'awesome-backstage.alicebob.io'
+    'https://awesome-backstage.alicebob.io'
 ]
 ALLOWED_HOSTS = [
     '*'
